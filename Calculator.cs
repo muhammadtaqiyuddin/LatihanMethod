d3d453166120e9a372b1c8d2de701093555e5565
mtaqiyuddin <mtaqiyuddin101@gmail.com>
2022-07-07 10:31:20 +0700

    Latihan Method

---
+++ Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMethod
+{
+    public class Calculator
+    {
+        public int Penjumlah(int a,int b)
+        {
+            return a+b;
+        }
+        public int Penjumlah(int a, int b,int c)
+        {
+            return a + b + c;
+        }
+
+        public int Pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+        public int Perkalian(int a, int b)
+        {
+            return a * b;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using  LatihanMethod;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        int hasil;
+
+        Calculator calculator = new Calculator();
+
+        hasil = calculator.Penjumlah(10, 2);
+        CetakHasil("Hasil penjumlah: " + hasil);
+
+        hasil = calculator.Penjumlah(10, 2, 3);
+        CetakHasil("Hasil penjumlah overload: " + hasil);
+
+        hasil = calculator.Pengurangan(7, 2);
+        CetakHasil("Hasil pengurangan: " + hasil);
+
+        hasil = calculator.Perkalian(5, 2);
+        CetakHasil("Hasil perkalian: " + hasil);
+
+    }
+    static void CetakHasil(string hasil)
+    {
+        Console.WriteLine(hasil);
+    }
+}
